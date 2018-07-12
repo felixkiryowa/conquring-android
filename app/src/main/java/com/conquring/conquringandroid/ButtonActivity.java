3cc46e3708d7abf8d4c27f316e66a9d2d3e1c74f
francis kiryowa <franciskiryowa68@gmail.com>
2018-07-12 18:53:41 +0300

    First Push To Git

---
+++ app/src/main/java/com/conquring/conquringandroid/ButtonActivity.java
@@ -0,0 +1,27 @@
+package com.conquring.conquringandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class ButtonActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_button);
+
+
+    }
+
+    public void optionOneSelected(View view) {
+        //Button btn = findViewById(R.id.Btn);
+        Toast.makeText(this,"Option 1 Selected",Toast.LENGTH_SHORT).show();
+    }
+
+    public void ButtonClickHere(View view) {
+        Toast.makeText(this,"click here",Toast.LENGTH_SHORT).show();
+    }
+}
