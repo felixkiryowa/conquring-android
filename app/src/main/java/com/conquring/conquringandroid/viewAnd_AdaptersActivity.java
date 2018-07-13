36e776e99c8b6feefa38dbb5d7453d4264fefc89
francis kiryowa <franciskiryowa68@gmail.com>
2018-07-13 19:33:45 +0300

    Adding a click event to items in a list view

--- app/src/main/java/com/conquring/conquringandroid/viewAnd_AdaptersActivity.java
+++ app/src/main/java/com/conquring/conquringandroid/viewAnd_AdaptersActivity.java
@@ -1,12 +1,19 @@
 package com.conquring.conquringandroid;
 
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class viewAnd_AdaptersActivity extends AppCompatActivity {
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,8 +24,20 @@ public class viewAnd_AdaptersActivity extends AppCompatActivity {
                 "KitKat"});
 
         ListView listView = (ListView) findViewById(R.id.list);
-
         listView.setAdapter(aAdpt);
+
+        //handling event lisetners
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> aView, View view, int position,long id) {
+                // We handle item click event
+                TextView tv = (TextView) view;
+                String text = (String) tv.getText();
+                Toast.makeText(viewAnd_AdaptersActivity.this, "You clicked on " + text, Toast.LENGTH_LONG).show();
+            }
+        });
+
+
 
     }
 }
